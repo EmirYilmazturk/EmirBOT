49f2dac7157386fbb7923a8934ac6bfd5901279f
EmirYilmazturk <44133479+EmirYilmazturk@users.noreply.github.com>
2018-11-10 19:55:40 +0300

    Add files via upload

---
+++ komutlar/kahveiç.js
@@ -0,0 +1,27 @@
+﻿const Discord = require('discord.js')
+
+
+exports.run = function(client, message) {
+    
+    const embed = new Discord.RichEmbed()
+        .setTitle("command.author.username + ' Kahve İçti'")
+        .setImage("https://cdn.discordapp.com/attachments/475678764643385344/485805902050623509/583.png")
+        .setColor(0xFF0000)
+
+        message.channel.send(embed)
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+};
+
+exports.help = {
+    name: 'kahveiç',
+    description: 'Kahve İçersiniz.',
+    usage: 'kahveiç'
+};
+
+
